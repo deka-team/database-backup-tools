70ec58e2da6590add0264ddff302ff8abbf9ed5e
riskihajar <riskihajar@gmail.com>
2024-08-21 20:49:41 +0800

    add log info

--- app/Actions/BackupDatabase.php
+++ app/Actions/BackupDatabase.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Arr;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Process;
 use Illuminate\Support\Facades\Storage;
 
@@ -70,13 +71,23 @@ class BackupDatabase
                 WHERE TABLE_SCHEMA = "$dbName" AND TABLE_NAME LIKE "pulse_%"
             SQL)) > 0;
 
+        $cmd1 = "{$mysqldump} --defaults-extra-file={$configFullPath} -h {$dbHost} -u {$dbUsername} {$dbName} {$listTable} > {$fullPathSql}";
+        $cmd2 = $pulseExists
+                    ? "{$mysqldump} --defaults-extra-file={$configFullPath} -h {$dbHost} -u {$dbUsername} {$dbName} pulse_aggregates pulse_entries pulse_values --no-data >> {$fullPathSql}"
+                    : null;
+        $cmd3 = "cat {$fullPathSql} | {$gzip} > $fullPathGz";
+        $cmd4 = "rm {$fullPathSql}";
+
+        Log::info("CMD 1 : {$cmd1}");
+        Log::info("CMD 2 : {$cmd2}");
+        Log::info("CMD 3 : {$cmd3}");
+        Log::info("CMD 4 : {$cmd4}");
+
         $output = Process::pipe(array_filter([
-            "{$mysqldump} --defaults-extra-file={$configFullPath} -h {$dbHost} -u {$dbUsername} {$dbName} {$listTable} > {$fullPathSql}",
-            $pulseExists
-                ? "{$mysqldump} --defaults-extra-file={$configFullPath} -h {$dbHost} -u {$dbUsername} {$dbName} pulse_aggregates pulse_entries pulse_values --no-data >> {$fullPathSql}"
-                : null ,
-            "cat {$fullPathSql} | {$gzip} > $fullPathGz",
-            "rm {$fullPathSql}",
+            $cmd1,
+            $cmd2,
+            $cmd3,
+            $cmd4,
         ]));
 
         if($output->successful()){
